fcba896893ee5e6f5caf99bbeab657b6200f408e
pascoal goncalves bruno <pascoal.goncalves.bruno@gmail.com>
2022-05-18 12:04:32 +0200

    rectif cube

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -3,13 +3,15 @@ import React from "react";
 const Cube = () => {
   return (
     <React.Fragment>
-      <div class="cube">
-        <div class="side" id="front"></div>
-        <div class="side" id="bottom"></div>
-        <div class="side" id="top"></div>
-        <div class="side" id="left"></div>
-        <div class="side" id="right"></div>
-        <div class="side" id="back"></div>
+      <div id="cubeee">
+        <div class="cube">
+          <div class="side" id="front"></div>
+          <div class="side" id="bottom"></div>
+          <div class="side" id="top"></div>
+          <div class="side" id="left"></div>
+          <div class="side" id="right"></div>
+          <div class="side" id="back"></div>
+        </div>
       </div>
     </React.Fragment>
   );
