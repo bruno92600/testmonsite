69bea89ec5a550410603f79039a2e63139160fb7
pascoal goncalves bruno <pascoal.goncalves.bruno@gmail.com>
2022-06-05 13:47:05 +0200

    rectif page photo

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,81 +16,32 @@ import foto10 from "../photo-8/IMG_5443.jpg";
 import foto11 from "../photo-8/IMG_5562.jpg";
 import foto12 from "../photo-8/IMG_5556.jpg";
 import "../styles/Main.css";
-import "../App.css"
+import "../App.css";
 
 const Main = () => {
   return (
     <React.Fragment>
+      <div className="saut"></div>
+      <h2 className="descriptfoto">Une de mes passions, la photo </h2>
+      <p className="descriptfotos">Qu'en pensez vous ?</p>
       <div className="fotos">
-        <div className="quatre">
-          <h3 className="titre">
-            du noir et blanc...
-            <img id="ecole" src={foto} alt="un collège a houilles" />
-          </h3>
-          <h3 className="titre">
-            du coloré...
-            <img id="ecole" src={fotoo} alt="un lampadaire dans le ciel" />
-          </h3>
+          <img id="ecole" src={foto} alt="un collège a houilles" />
+          <img id="ecole" src={fotoo} alt="un lampadaire dans le ciel" />
+          <img id="ecole" src={fotooo} alt="branches de végétation" />
+          <img id="ecole" src={fotoooo} alt="tulipe rouge" />
+          <img id="ecole" src={foto1} alt="écriture sur un mur" />
+          <img id="ecole" src={foto2} alt="ville de Porto vu de nuit" />
+          <img id="ecole" src={foto3} alt="couché de soleil dans la ville de Porto" />
+          <img id="ecole" src={foto4} alt="Musée des Tuileries à Paris" />
+          <img id="ecole" src={foto5} alt="Vu sur le pont de la ville de Porto" />
+          <img id="ecole" src={foto6} alt="Vu sur la rivière du Douro dans la ville de porto" />
+          <img id="ecole" src={foto7} alt="Une feuille d'automne" />
+          <img id="ecole" src={foto8} alt="La mer très calme dans des teintes irisée" />
+          <img id="ecole" src={foto9} alt="Maison de pécheur" />
+          <img id="ecole" src={foto10} alt="Cascade d'eau dans le Gérês au Portugal" />
+          <img id="ecole" src={foto11} alt="Mouette volant au dessus de la mer" />
+          <img id="ecole" src={foto12} alt="Église à étretat" />
         </div>
-        <div className="cinq">
-          <h3 className="titre">
-            de la végétation...
-            <img id="ecole" src={fotooo} alt="végétation" />
-          </h3>
-          <h3 className="titre">
-            des fleurs...
-            <img id="ecole" src={fotoooo} alt="tulipe rouge" />
-          </h3>
-        </div>
-        <div className="cinq">
-          <h3 className="titre">
-            <img id="ecole" src={foto1} alt="végétation" />
-          </h3>
-          <h3 className="titre">
-            <img id="ecole" src={foto2} alt="tulipe rouge" />
-          </h3>
-        </div>
-        <div className="cinq">
-          <h3 className="titre">
-            <img id="ecole" src={foto3} alt="végétation" />
-          </h3>
-          <h3 className="titre">
-            <img id="ecole" src={foto4} alt="tulipe rouge" />
-          </h3>
-        </div>
-        <div className="cinq">
-          <h3 className="titre">
-            <img id="ecole" src={foto5} alt="végétation" />
-          </h3>
-          <h3 className="titre">
-            <img id="ecole" src={foto6} alt="tulipe rouge" />
-          </h3>
-        </div>
-        <div className="cinq">
-          <h3 className="titre">
-            <img id="ecole" src={foto7} alt="végétation" />
-          </h3>
-          <h3 className="titre">
-            <img id="ecole" src={foto8} alt="tulipe rouge" />
-          </h3>
-        </div>
-        <div className="cinq">
-          <h3 className="titre">
-            <img id="ecole" src={foto9} alt="végétation" />
-          </h3>
-          <h3 className="titre">
-            <img id="ecole" src={foto10} alt="tulipe rouge" />
-          </h3>
-        </div>
-        <div className="cinq">
-          <h3 className="titre">
-            <img id="ecole" src={foto11} alt="végétation" />
-          </h3>
-          <h3 className="titre">
-            <img id="ecole" src={foto12} alt="tulipe rouge" />
-          </h3>
-        </div>
-      </div>
     </React.Fragment>
   );
 };
